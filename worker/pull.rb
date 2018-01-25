821b64488f53f7b100626469420076a43768ff1b
Ryan Barth <ryan@barth.tech>
2018-01-25 10:29:53 -0800

    adapt for handling associations, couple bug fixes

--- worker/pull.rb
+++ worker/pull.rb
@@ -3,45 +3,62 @@ require_relative '../lib/async'
 module ShopifyCache
   include Async
 
-  def pull_products
-    pull_entity(ShopifyAPI::Product, Product)
-    variants = Product.all.pluck(:variants).flatten
-    puts "adding #{variants.count} product variants"
-    variants.each do |variant|
-      ProductVariant.find_or_initialize_by(id: variant['id'])
-        .update(variant)
+  # Caches both Products and the associated ProductVariants
+  def self.pull_products
+    pull_entity(ShopifyAPI::Product, Product) do |product_data|
+      variants = product_data['variants'].map do |variant|
+        pv = ProductVariant.find_or_initialize_by(id: variant['id'])
+        pv.assign_attributes(variant)
+        pv
+      end
+      product_data.merge('variants' => variants)
     end
   end
 
-  def pull_order
+  def self.pull_orders
     pull_entity ShopifyAPI::Order, ShopifyOrder
   end
 
-  def pull_collects
+  def self.pull_collects
     pull_entity ShopifyAPI::Collect, Collect
   end
 
-  def pull_custom_collections
+  def self.pull_custom_collections
     pull_entity ShopifyAPI::CustomCollection, CustomCollection
   end
 
-  def pull_all
-    pull_entity ShopifyAPI::Order, ShopifyOrder
-    pull_entity ShopifyAPI::CustomCollection, CustomCollection
-    pull_entity ShopifyAPI::Collect, Collect
+  def self.pull_all
+    pull_collects
+    pull_custom_collections
+    pull_orders
     pull_products
   end
 
-  private
-
-  def pull_entity(api_entity, db_entity, query_params = {})
+  # Used to cache a Shopify Entity to a local database.
+  #
+  # Arguments:
+  #
+  # * api_entity: The ShopifyAPI class to query
+  # * db_entity: The ActiveRecord class to save into
+  # * query_params: a hash corresponting to the index query params of the
+  # ShopifyAPI::Entity::where function. Useful for limiting the scope of the
+  # cache update
+  # * &block(data): data is the attributes hash of the shopify entity. The result
+  # of the block is passed to the ActiveRecord::Base#update function.
+  #
+  # Returns: nil
+  def self.pull_entity(api_entity, db_entity, query_params = {})
     count = api_entity.count
     limit = 250
     pages = count.fdiv(limit).ceil
-    (1..pages).each do |page|
+    objects = (1..pages).flat_map do |page|
       where_args = query_params.merge(limit: limit, page: page)
-      objects = ShopifyAPI.throttle { api_entity.where(where_args) }
-      objects.each{|order| db_entity.find_or_initialize_by(id: order.id).update(order.attributes.as_json)}
+      ShopifyAPI.throttle { api_entity.where(where_args) }
     end
+    objects.each do |object|
+      data = block_given? ? yield(object.attributes.as_json) : object.attributes.as_json
+      db_entity.find_or_initialize_by(id: object.id).update(data)
+    end
+    return
   end
 end
