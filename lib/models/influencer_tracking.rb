d7706bbf0ff8d4bb6b0505de0ccbac5be39aa1da
Ryan Barth <ryan@barth.tech>
2018-02-16 13:36:43 -0800

    update tracking and orders relations

--- lib/models/influencer_tracking.rb
+++ lib/models/influencer_tracking.rb
@@ -2,7 +2,8 @@ class InfluencerTracking < ActiveRecord::Base
   include ApplicationRecord
 
   self.table_name = 'influencer_tracking'
-  belongs_to :order, class_name: 'InfluencerOrder', foreign_key: 'order_id'
+  has_many(:orders, class_name: 'InfluencerOrder', foreign_key: 'name',
+           primary_key: 'order_name')
   has_one :influencer, through: 'order'
 
   def email_data
