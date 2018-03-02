9ec1f4655696bed7e8892b484a4a9af496a10282
Ryan Barth <ryan@barth.tech>
2018-03-02 11:04:26 -0800

    set default value on tax_code migration

--- db/migrate/20180301225913_add_tax_code_to_product_variants.rb
+++ db/migrate/20180301225913_add_tax_code_to_product_variants.rb
@@ -1,5 +1,5 @@
 class AddTaxCodeToProductVariants < ActiveRecord::Migration[5.1]
   def change
-    add_column :shopify_product_variants, :tax_code, :string, null: false
+    add_column :shopify_product_variants, :tax_code, :string, null: false, default: ''
   end
 end
